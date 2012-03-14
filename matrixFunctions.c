9118f2d6dd05e66c8946129244c24aef6c88b5d2
Shir Yehoshua <shiryehoshua@gmail.com>
2012-03-14 13:54:45 -0500

    correct (kindof) orbits

--- matrixFunctions.c
+++ matrixFunctions.c
@@ -671,15 +671,17 @@ void updateScene(GLfloat time, GLfloat dt)
 
   // update each planet
   for (gi = 0; gi < gctx->geomNum; gi++) {
-    rotate_model_ith(gctx->geom[gi], gctx->geom[gi]->axialThetaPerSec * dt, 1);
+    rotate_model_ith(gctx->geom[gi], gctx->geom[gi]->axialThetaPerSec * dt / 20, 1);
 
     if (gi != 0) {
-      orbit(gctx->geom[gi], gctx->geom[gi]->orbitAxis, gctx->geom[gi]->orbitThetaPerSec * dt);
+      orbit(gctx->geom[gi], gctx->geom[gi]->orbitAxis, (1/gctx->geom[gi]->orbitThetaPerSec) * dt * 10);
     }
   }
 
   gctx->time += dt;
   if (gctx->time >= 5.0) { gctx->time = 0.0f; }
+
+  fprintf(stderr, "time: %f\n", gctx->time);
 }
 
 /* Normalize homogenous coordinates */
